5e644d7c9ec4167e0a68e735c2de32a9e720c312
Fábio Souza <fabio.souza@vaipe.com.br>
2021-12-15 22:05:16 -0300

    feat: adds string calculator support to custom delimiter

--- src/Entities/StringCalculator.php
+++ src/Entities/StringCalculator.php
@@ -12,8 +12,17 @@ final class StringCalculator
             return 0;
         }
 
+        $delimiter = ',';
+
+        if (str_starts_with($numbers, '//')) {
+            $positionCustomDelimiter = strpos($numbers, '\n');
+            $splitNumbers = str_split($numbers, $positionCustomDelimiter);
+            $delimiter = str_replace('//', '', $splitNumbers[0]);
+            $numbers = str_replace('//' . $delimiter, '', $numbers);
+        }
+
         $numbers = str_replace('\n', ' ', $numbers);
 
-        return array_sum(explode(',', $numbers));
+        return array_sum(preg_split("/[\{$delimiter}]+/", $numbers));
     }
 }
--- tests/Unit/Entities/StringCalculatorTest.php
+++ tests/Unit/Entities/StringCalculatorTest.php
@@ -49,4 +49,18 @@ class StringCalculatorTest extends TestCase
         self::assertSame(6, $result1);
         self::assertSame(7, $result2);
     }
+
+    /** @test */
+    public function itShouldSupportACustomDelimiter(): void
+    {
+        $result1 = $this->stringCalculator->add('//;\n1;3;4');
+        $result2 = $this->stringCalculator->add('//$\n1$2$3');
+        $result3 = $this->stringCalculator->add('//@\n2@3@8');
+        $result4 = $this->stringCalculator->add('//@\n2@\n3@9');
+
+        self::assertSame(8, $result1);
+        self::assertSame(6, $result2);
+        self::assertSame(13, $result3);
+        self::assertSame(14, $result4);
+    }
 }
